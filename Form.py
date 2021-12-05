1bbdf1cf312ae17a3f257eed38933967de7b61ac
James <jahood@student.fullsail.edu>
2021-12-05 01:57:47 -0600

    Cleaned up program and inverted yaw output for reading objects away from screen

--- Form.py
+++ Form.py
@@ -159,11 +159,11 @@ class Form:
                     color = [114, 42, 203]
                 cv2.circle(frame, (self.cv.pred_pts[i][0], self.cv.pred_pts[i][1]), (int(self.cv.targetData[0][2] / 2)), color, 2)
         if len(self.cv.pred_pts) > 0 and intercept is not None:  #CMM commands input(James)                  
-                self.aim.cmmpitch(self.cv.interceptData[0]) 
+                self.aim.cmmpitch(self.cv.interceptData[1]) 
                 #self.aim.cmmpitch(self.cv.targetData[0][1])
-                self.aim.cmmyaw(self.cv.interceptData[1])
+                self.aim.cmmyaw(self.cv.interceptData[0])
                 #self.aim.cmmyaw(self.cv.targetData[0][0])
-                self.aim.cmmfire(self.cv.interceptData[2])
+                #self.aim.cmmfire(self.cv.interceptData[2])
         if ret:
             self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
         self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
